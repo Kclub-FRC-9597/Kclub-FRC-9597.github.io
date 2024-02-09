962735e4e4b8c297e70e7ae99de079a8693ebcb2
Brook <yinpeiyang@126.com>
2024-02-09 18:55:10 +0800

    remove pull.bat and push.bat which is designed for subtree

--- pull.bat
+++
@@ -1,2 +0,0 @@
-git subtree pull --prefix=website/content https://github.com/Kclub-FRC-9597/Kclub-FRC-9597.github.io.git main
-git subtree pull --prefix=website/output https://github.com/Kclub-FRC-9597/Kclub-FRC-9597.github.io.git Docs
